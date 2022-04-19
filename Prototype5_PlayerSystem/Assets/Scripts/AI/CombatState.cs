bc77a0322a71ea83bbbc6af02b24d8ae6849fae5
u1067294 <u1067294@utah.edu>
2022-04-19 03:51:31 -0600

    attack animation and state pattern done

--- Prototype5_PlayerSystem/Assets/Scripts/AI/CombatState.cs
+++ Prototype5_PlayerSystem/Assets/Scripts/AI/CombatState.cs
@@ -9,7 +9,10 @@ public class CombatState : AIState
     public override AIState Tick(EnemyManager em, EnemyStats es, EnemyAnimationManager eam)
     {
         em.distanceFromTarget = Vector3.Distance(em.character.transform.position, em.transform.position);
-
+        if(em.isPerformingAction)
+        {
+            eam.anim.SetFloat("Vertical", 0, 0.1f, Time.deltaTime);
+        }
         if (em.currentRecoveryTime <= 0 && em.distanceFromTarget <= em.maximumAttackRange)
         {
             return attackState;
